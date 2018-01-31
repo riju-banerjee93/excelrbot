2e6a6802ccbd2cbc1f166ab8ec20765986ea01d7
Sayantan Banerjee <riju.banerjee93@gmail.com>
2018-01-31 21:32:11 +0530

    1st

--- excelrbot.py
+++ excelrbot.py
@@ -31,19 +31,36 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    a = req.get("result").get("parameters").get("course")
-    sheet = client.open("sheetdemo").worksheet("Sheet1")
+    if req.get("result").get("action") == "user.query":
 
-    x = {"DATA SCIENCE": sheet.range('B2:J2'), "TABLEAU": sheet.range('B3:J3'),"BIG DATA HADOOP":sheet.range('B4:J4'),"ADVANCED ANALYTICS":sheet.range('B5:J5'),
-         "PROJECT MANAGEMENT PROFESSIONAL":sheet.range('B6:J6'),"AGILE CERTIFIED PROFESSIONAL":sheet.range('B7:J7'),"ITIL FOUNDATION":sheet.range('B8:J8'),
-         "ITIL INTERMEDIATE":sheet.range('B9:J9'),"PRINCE 2 FOUNDATION": sheet.range('B10:J10'),"PRINCE 2 PRACTITIONER":sheet.range('B11:J11'),
-         "LEAN SIX SIGMA GREEN BELT":sheet.range('B12:J12'),"LEAN SIX SIGMA BLACK BELT": sheet.range('B13:J13'),"CAPM": sheet.range('B14:J14'),
-         "MSP":sheet.range('B15:J15'),"Internet of Things":sheet.range('B16:J16'),"Amazon Web Servies": sheet.range('B17:J17')}
-    list1 = []
-    for cell in x[a]:
-        list1.append(cell.value)
+        a = req.get("result").get("parameters").get("course")
+        sheet = client.open("sheetdemo").worksheet("Sheet1")
 
-    speech = "We offer " +list1[0]+ " course at a cost of Rs. "+list1[1]+" (excluding taxes). The training duaration is "+list1[7]+" hours. We provide "+list1[8]+" mode of training for this course."
+        x = {"DATA SCIENCE": sheet.range('B2:J2'), "TABLEAU": sheet.range('B3:J3'),"BIG DATA HADOOP":sheet.range('B4:J4'),"ADVANCED ANALYTICS":sheet.range('B5:J5'),
+             "PROJECT MANAGEMENT PROFESSIONAL":sheet.range('B6:J6'),"AGILE CERTIFIED PROFESSIONAL":sheet.range('B7:J7'),"ITIL FOUNDATION":sheet.range('B8:J8'),
+            "ITIL INTERMEDIATE":sheet.range('B9:J9'),"PRINCE 2 FOUNDATION": sheet.range('B10:J10'),"PRINCE 2 PRACTITIONER":sheet.range('B11:J11'),
+            "LEAN SIX SIGMA GREEN BELT":sheet.range('B12:J12'),"LEAN SIX SIGMA BLACK BELT": sheet.range('B13:J13'),"CAPM": sheet.range('B14:J14'),
+            "MSP":sheet.range('B15:J15'),"Internet of Things":sheet.range('B16:J16'),"Amazon Web Servies": sheet.range('B17:J17')}
+        list1 = []
+        for cell in x[a]:
+            list1.append(cell.value)
+
+        speech = "We offer " +list1[0]+ " course at a cost of Rs. "+list1[1]+" (excluding taxes). The training duaration is "+list1[7]+" hours. We provide "+list1[8]+" mode of training for this course."
+
+    elif req.get("result").get("action") == "what.price":
+        b = req.get("result").get("parameters").get("course")
+        sheet = client.open("sheetdemo").worksheet("Sheet1")
+
+        y = {"DATA SCIENCE": sheet.acell("F2").value,"TABLEAU":sheet.acell("F3").value,"BIG DATA HADOOP":sheet.acell("F4").value,
+             "ADVANCED ANALYTICS":sheet.acell("F5").value,"PROJECT MANAGEMENT PROFESSIONAL":sheet.acell("F6").value,"AGILE CERTIFIED PROFESSIONAL":sheet.acell("F7").value,
+             "ITIL FOUNDATION":sheet.acell("F8").value,"ITIL INTERMEDIATE":sheet.acell("F9").value,"PRINCE 2 FOUNDATION":sheet.acell("F10").value,"PRINCE 2 PRACTITIONER":sheet.acell("F11").value,
+             "LEAN SIX SIGMA GREEN BELT":sheet.acell("F12").value,"LEAN SIX SIGMA BLACK BELT":sheet.acell("F13").value,"CAPM":sheet.acell("F14").value,
+             "MSP":sheet.acell("F15").value,"Internet of Things":sheet.acell("F16").value,"Amazon Web Servies":sheet.acell("F17").value}
+
+
+        speech = "Its Rs. "+y[b]
+
+
 
 
     return {
