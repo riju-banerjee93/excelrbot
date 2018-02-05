0fe7bcb955ce3c79c9304997f9fb4e52a3ceb06c
Sayantan Banerjee <riju.banerjee93@gmail.com>
2018-02-05 15:26:14 +0530

    1st

--- excelrbot.py
+++ excelrbot.py
@@ -81,8 +81,10 @@ def makeWebhookResult(req):
 
 
     elif req.get("result").get("action") == "user.query.price":
-        d = req.get("result").get("contexts")["course"]
-        print(d+"2")
+        d = req.get("result").get("contexts")
+        print(d)
+        d = d[0].get("course")
+        print(d)
         sheet = client.open("sheetdemo").worksheet("Sheet1")
 
         y = {"DATA SCIENCE": sheet.acell("F2").value, "TABLEAU": sheet.acell("F3").value,
